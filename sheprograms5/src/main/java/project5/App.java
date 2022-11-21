c70127080f1bc054e1a21c2d88db0464dc65dc7b
Erika Ledesma <erika.jasmine06@gmail.com>
2022-11-20 23:11:12 -0800

    added maven files

---
+++ sheprograms5/src/main/java/project5/App.java
@@ -0,0 +1,13 @@
+package project5;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
