128e3e84157e7c955bde4bf1bea3a5255186e8d2
Erika Ledesma <erika.jasmine06@gmail.com>
2022-12-01 22:28:11 -0800

    added more source code

---
+++ sheprograms5/src/main/java/project5/30.21.java
@@ -0,0 +1,14 @@
+// // @author Frank M. Carrano, Timothy M. Henry
+// // @version 5.0
+// protected void resetVertices()
+// {
+//    Iterator<VertexInterface<T>> vertexIterator = vertices.getValueIterator();
+//    while (vertexIterator.hasNext())
+//    {
+//       VertexInterface<T> nextVertex = VertexIterator.next();
+//       nextVertex.unvisit();
+//       nextVertex.setCost(0);
+//       nextVertex.setPredecessor(null);
+//    } // end while
+// } // end resetVertices
+
