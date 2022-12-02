470bf7acdec628cde1915752042324ab15a4cc7c
tiff178 <tiffmt817@gmail.com>
2022-12-01 23:45:58 -0800

    uncomment graphinterface

--- sheprograms5/src/main/java/project5/GraphInterface.java
+++ sheprograms5/src/main/java/project5/GraphInterface.java
@@ -1,11 +1,11 @@
-// package project5;
-// /**
-//    An interface of methods that create, manipulate, and process a graph.
+package project5;
+/**
+   An interface of methods that create, manipulate, and process a graph.
    
-//    @author Frank M. Carrano
-//    @author Timothy M. Henry
-//    @version 5.0
-// */
-// public interface GraphInterface<T> extends BasicGraphInterface<T>, GraphAlgorithmsInterface<T>
-// {
-// } // end GraphInterface
+   @author Frank M. Carrano
+   @author Timothy M. Henry
+   @version 5.0
+*/
+public interface GraphInterface<T> extends BasicGraphInterface<T>, GraphAlgorithmsInterface<T>
+{
+} // end GraphInterface
