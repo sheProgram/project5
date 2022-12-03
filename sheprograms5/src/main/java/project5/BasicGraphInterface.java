d427a05c5539966162f488f65d35736e57a10a7b
Mohraiel Matta <mohraielmatta@gmail.com>
2022-12-03 15:55:20 -0800

    combined graphinterface and basicgraphint

--- sheprograms5/src/main/java/project5/BasicGraphInterface.java
+++ sheprograms5/src/main/java/project5/BasicGraphInterface.java
@@ -57,5 +57,16 @@ public interface BasicGraphInterface<T>
 
    /** Removes all vertices and edges from this graph resulting in an empty graph. */
    public void clear();
+/**
+   An interface of methods that create, manipulate, and process a graph.
+   
+   @author Frank M. Carrano
+   @author Timothy M. Henry
+   @version 5.0
+*/
+public interface GraphInterface<T> extends BasicGraphInterface<T>, 
+                                           GraphAlgorithmsInterface<T>
+{
+} // end GraphInterface
 } // end BasicGraphInterface
 
