4366ecc123071097392ea09de12ad90a756f6caa
Mohraiel Matta <mohraielmatta@gmail.com>
2022-12-04 00:46:28 -0800

    OMGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG

--- sheprograms5/src/main/java/project5/main5.java
+++ sheprograms5/src/main/java/project5/main5.java
@@ -2,31 +2,32 @@ package project5;
 public class main5 {
     public static void main (String[] args){
     //The graph: {V, E}
-        Graph graph = new Graph<>();
-        graph.addVertex("A");
-        graph.addVertex("B");
-        graph.addVertex("C");
-        graph.addVertex("D");
-        graph.addVertex("E");
-        graph.addVertex("F");
-        graph.addVertex("G");
-        graph.addVertex("H");
-        graph.addVertex("I");
-        //Graph <String> graph = new Graph<>(graphSize);
-        //QueueInterface <String> breadthFirst = graph.getBreadthFirstTraversal("A");
-        graph.addEdge("A", "B");
-        graph.addEdge("A", "D");
-        graph.addEdge("A", "E");
-        graph.addEdge("B", "E");
-        graph.addEdge("D", "G");
-        graph.addEdge("E", "F");
-        graph.addEdge("E", "H");
-        graph.addEdge("G", "H");
-        graph.addEdge("F", "C");
-        graph.addEdge("F", "H");
-        graph.addEdge("H", "I");
-        graph.addEdge("C", "B");
-        graph.addEdge("I", "F");
+        GraphInterface<Character> pgraph = new Graph<>();
+
+        //Add vertices to the graph
+        pgraph.addVertex('A');
+        pgraph.addVertex('B');
+        pgraph.addVertex('C');
+        pgraph.addVertex('D');
+        pgraph.addVertex('E');
+        pgraph.addVertex('F');
+        pgraph.addVertex('G');
+        pgraph.addVertex('H');
+        pgraph.addVertex('I');
+
+        //Connect the vertices
+        pgraph.addEdge('A', 'B');
+        pgraph.addEdge('A', 'E');
+        pgraph.addEdge('A', 'D');
+        pgraph.addEdge('B', 'E');
+        pgraph.addEdge('D', 'G');
+        pgraph.addEdge('E', 'F');
+        pgraph.addEdge('E', 'H');
+        pgraph.addEdge('G', 'H');
+        pgraph.addEdge('H', 'I');
+        pgraph.addEdge('F', 'H');
+        pgraph.addEdge('F', 'C');
+        pgraph.addEdge('C', 'B');
 
     
     }
