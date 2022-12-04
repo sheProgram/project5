abb6c70c68cf94a0fca4b8ec308063b0905447ef
Mohraiel Matta <mohraielmatta@gmail.com>
2022-12-03 22:27:45 -0800

    added remove method

--- sheprograms5/src/main/java/ADTPackage/LinkedListWithIterator.java
+++ sheprograms5/src/main/java/ADTPackage/LinkedListWithIterator.java
@@ -138,8 +138,29 @@ public class LinkedListWithIterator<T> implements ListWithIteratorInterface<T>
 
     @Override
     public T remove(int givenPosition) {
-        // TODO Auto-generated method stub
-        return null;
+      T result = null;                           // Return value
+      if ((givenPosition >= 1) && (givenPosition <= numberOfEntries))
+      {
+         // Assertion: !isEmpty()
+         if (givenPosition == 1)                 // Case 1: Remove first entry
+         {
+            result = firstNode.getData();        // Save entry to be removed
+            firstNode = firstNode.getNextNode(); // Remove entry
+         }
+         else                                    // Case 2: Not first entry
+         {
+            Node nodeBefore = getNodeAt(givenPosition - 1);
+            Node nodeToRemove = nodeBefore.getNextNode();
+            result = nodeToRemove.getData();    // Save entry to be removed
+            Node nodeAfter = nodeToRemove.getNextNode();
+            nodeBefore.setNextNode(nodeAfter);  // Remove entry
+         } // end if
+         numberOfEntries--;                     // Update count
+         return result;                         // Return removed entry
+      }
+      else
+         throw new IndexOutOfBoundsException(
+                   "Illegal position given to remove operation.");
     }
 
     @Override
