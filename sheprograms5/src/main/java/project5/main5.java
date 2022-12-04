20be73aad2bcd1895766dea77ca89580a04ffe3a
Erika Ledesma <erika.jasmine06@gmail.com>
2022-12-04 00:41:37 -0800

    here you go mo <3

--- sheprograms5/src/main/java/project5/main5.java
+++ sheprograms5/src/main/java/project5/main5.java
@@ -1,9 +1,33 @@
 package project5;
-
 public class main5 {
     public static void main (String[] args){
-        int graphSize = 9;
+    //The graph: {V, E}
+        Graph graph = new Graph<>();
+        graph.addVertex("A");
+        graph.addVertex("B");
+        graph.addVertex("C");
+        graph.addVertex("D");
+        graph.addVertex("E");
+        graph.addVertex("F");
+        graph.addVertex("G");
+        graph.addVertex("H");
+        graph.addVertex("I");
         //Graph <String> graph = new Graph<>(graphSize);
         //QueueInterface <String> breadthFirst = graph.getBreadthFirstTraversal("A");
+        graph.addEdge("A", "B");
+        graph.addEdge("A", "D");
+        graph.addEdge("A", "E");
+        graph.addEdge("B", "E");
+        graph.addEdge("D", "G");
+        graph.addEdge("E", "F");
+        graph.addEdge("E", "H");
+        graph.addEdge("G", "H");
+        graph.addEdge("F", "C");
+        graph.addEdge("F", "H");
+        graph.addEdge("H", "I");
+        graph.addEdge("C", "B");
+        graph.addEdge("I", "F");
+
+    
     }
 }
