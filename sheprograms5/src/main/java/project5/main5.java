c489c0b329a61fa737c7b64f4ed856223543d0e1
tiff178 <tiffmt817@gmail.com>
2022-12-03 20:41:53 -0800

    main file to run

--- sheprograms5/src/main/java/project5/main5.java
+++ sheprograms5/src/main/java/project5/main5.java
@@ -1,5 +1,11 @@
 package project5;
 
+import ADTPackage.QueueInterface;
+
 public class main5 {
-    
+    public static void main (String[] args){
+        int graphSize = 9;
+        //Graph <String> graph = new Graph<>(graphSize);
+        //QueueInterface <String> breadthFirst = graph.getBreadthFirstTraversal("A");
+    }
 }
