87c80803989819803aebe2cfd208e9e282f23ed4
Erika Ledesma <erika.jasmine06@gmail.com>
2022-12-04 18:09:35 -0800

    fixed LinkedStack

--- sheprograms5/src/main/java/ADTPackage/LinkedStack.java
+++ sheprograms5/src/main/java/ADTPackage/LinkedStack.java
@@ -5,80 +5,91 @@ import java.util.EmptyStackException;
     @author Frank M. Carrano and Timothy M. Henry
     @version 5.0
 */
-public final class LinkedStack<T> implements StackInterface<T>
+public class LinkedStack<T> implements StackInterface<T>
 {
-	private T[] stack;
-   private int topIndex;
-
    private Node topNode; // References the first node in the chain
   
    public LinkedStack()
    {
       topNode = null;
    } // end default constructor
-  
-//  < Implementations of the stack operations go here. >
-//  . . .
-private boolean integrityOK = false;
-private void checkIntegrity()
-{
-   if (!integrityOK)
-      throw new SecurityException("ArrayBag object is corrupt.");
-} // end checkIntegrity
-	private class Node
-	{
-      private Node next; // Link to next node
-      
-      private Node(T dataPortion)
-      {
-         this(dataPortion, null);
-      } // end constructor
-      
-      private Node(T dataPortion, Node linkPortion)
-      {
-         next = linkPortion;
-      } // end constructor
-      
-      private Node getNextNode()
-      {
-         return next;
-      } // end getNextNode
-	} // end Node
 
-    public void push(T newEntry) {
+   /**
+     * Removes all entries from this bag.
+     * 
+     * @author Frank M. Carrano, Timothy M. Henry
+     * @version 5.0
+     */
+    public void clear() {
+      topNode = null;
+  } // end clear
+
+    // @author Frank M. Carrano, Timothy M. Henry
+    // @version 5.0
+
+  public void push(T newEntry) {
       Node newNode = new Node(newEntry, topNode);
-      topNode = newNode; 
-    }
+      topNode = newNode;
+  } // end push
 
-    public T pop() {
-      T top = peek();  // Might throw EmptyStackException
+  // @author Frank M. Carrano, Timothy M. Henry
+  // @version 5.0
+  public T peek() {
+      if (isEmpty())
+          throw new EmptyStackException();
+      else
+          return topNode.getData();
+  } // end peek
+
+  // @author Frank M. Carrano, Timothy M. Henry
+  // @version 5.0
+  public T pop() {
+      T top = peek(); // Might throw EmptyStackException
 
       // Assertion: topNode != null
       topNode = topNode.getNextNode();
-   
+
       return top;
-    }
+  } // end pop
 
-    @Override
-    public T peek() {
-      checkIntegrity();
-      if (isEmpty())
-         throw new EmptyStackException();
-      else
-      return stack[topIndex];
-   } // end peek
+  /**
+   * @author Frank M. Carrano, Timothy M. Henry
+   * @version 5.0
+   */
+  public boolean isEmpty() {
+      return topNode == null;
+  } // end isEmpty
 
-    @Override
-    public boolean isEmpty() {
-      return topIndex < 0;
-   } // end isEmpty
+  // @version 5.0 */
+  private class Node {
+      private T data; // Entry in bag
+      private Node next; // Link to next node
 
-    @Override
-    public void clear() {
-      checkIntegrity();
-      while (topIndex > -1){
-         stack[topIndex] = null;
-         topIndex--;
-      } // end while
-   } // end clear
+      private Node(T dataPortion) {
+          this(dataPortion, null);
+      } // end constructor
+
+      private Node(T dataPortion, Node nextNode) {
+          data = dataPortion;
+          next = nextNode;
+      } // end constructor
+
+      private T getData() {
+          return data;
+      } // end getData
+
+      private void setData(T newData) {
+          data = newData;
+      } // end setData
+
+      private Node getNextNode() {
+          return next;
+      } // end getNextNode
+
+      private void setNextNode(Node nextNode) {
+          next = nextNode;
+      } // end setNextNode
+
+  } // end Node
+
 } // end LinkedStack
