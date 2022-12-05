df282214874d2ac1bd2d361b79f9f522d2582b93
tiff178 <tiffmt817@gmail.com>
2022-12-04 23:34:20 -0800

    test cases work??

--- sheprograms5/src/test/java/project5/main5Test.java
+++ sheprograms5/src/test/java/project5/main5Test.java
@@ -47,4 +47,42 @@ public class main5Test
         }
 
     }
+
+    @Test
+    public void getDepthFirstTraversal() {
+        Graph <String> graph1 = new Graph<>();
+        //creating the vertexes
+        graph1.addVertex("A");
+        graph1.addVertex("B");
+        graph1.addVertex("C");
+        graph1.addVertex("D");
+        graph1.addVertex("E");
+        graph1.addVertex("F");
+        graph1.addVertex("G");
+        graph1.addVertex("H");
+        graph1.addVertex("I");
+                
+        //creating the edges between them in order
+        graph1.addEdge("A", "B");
+        graph1.addEdge("A", "D");
+        graph1.addEdge("A", "E");
+        graph1.addEdge("B", "E");
+        graph1.addEdge("D", "G");
+        graph1.addEdge("E", "F");
+        graph1.addEdge("E", "H");
+        graph1.addEdge("G", "H");
+        graph1.addEdge("F", "C");
+        graph1.addEdge("F", "H");
+        graph1.addEdge("H", "I");
+        graph1.addEdge("C", "B");
+        graph1.addEdge("I", "F");
+
+        QueueInterface<String> depthFirstTraversal = graph1.getDepthFirstTraversal("A");
+        System.out.println("DFT:  \n-------------------------------------------------------");
+        while(!depthFirstTraversal.isEmpty())
+        {
+            System.out.println(depthFirstTraversal.dequeue() + "-> ");
+        }
+
+    }
 }
