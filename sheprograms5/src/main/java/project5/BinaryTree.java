3c10704168dacb2908b4e97c053cfe2735570a81
tiff178 <tiffmt817@gmail.com>
2022-12-05 16:55:29 -0800

    attempt trees

---
+++ sheprograms5/src/main/java/project5/BinaryTree.java
@@ -0,0 +1,117 @@
+package project5;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+import ADTPackage.*; // Needed by tree iterators
+
+/**
+   A class that implements the ADT binary tree.
+   
+   @author Frank M. Carrano
+   @author Timothy M. Henry
+   @version 5.0
+*/
+public class BinaryTree<T> implements BinaryTreeInterface<T>
+{
+   private BinaryNode<T> root;
+
+   public BinaryTree()
+   {
+      root = null;
+   } // end default constructor
+
+   public BinaryTree(T rootData)
+   {
+      root = new BinaryNode<>(rootData);
+   } // end constructor
+
+   public BinaryTree(T rootData, BinaryTree<T> leftTree, BinaryTree<T> rightTree)
+   {
+      initializeTree(rootData, leftTree, rightTree);
+   } // end constructor
+
+    public void setTree(T rootData, BinaryTreeInterface<T> leftTree,
+                                   BinaryTreeInterface<T> rightTree)
+   {
+      initializeTree(rootData, (BinaryTree<T>)leftTree,
+                               (BinaryTree<T>)rightTree);
+   } // end setTree
+
+	private void initializeTree(T rootData, BinaryTree<T> leftTree,
+	                                        BinaryTree<T> rightTree)
+	{
+      // < FIRST DRAFT - See Segments 25.4 - 25.7 for improvements. >
+      root = new BinaryNode<T>(rootData);
+      
+      if (leftTree != null)
+         root.setLeftChild(leftTree.root);
+      
+      if (rightTree != null)
+         root.setRightChild(rightTree.root);
+	} // end initializeTree
+
+    @Override
+    public T getRootData() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public int getHeight() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public int getNumberOfNodes() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void clear() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public Iterator<T> getPreorderIterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterator<T> getPostorderIterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterator<T> getInorderIterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterator<T> getLevelOrderIterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void setRootData(T rootData) {
+        // TODO Auto-generated method stub
+        
+    }
+
+/* Implementations of setRootData, getRootData, getHeight, getNumberOfNodes,
+   isEmpty, clear, and the methods specified in TreeIteratorInterface are here.
+   . . . */
+
+} // end BinaryTree
---
+++ sheprograms5/src/main/java/project5/TreeInterface.java
@@ -0,0 +1,17 @@
+package project5;
+
+/**
+   An interface of basic methods for the ADT tree.
+ 
+   @author Frank M. Carrano
+   @author Timothy M. Henry
+   @version 5.0
+*/
+public interface TreeInterface<T>
+{
+   public T getRootData();
+   public int getHeight();
+   public int getNumberOfNodes();
+   public boolean isEmpty();
+   public void clear();
+} // end TreeInterface
