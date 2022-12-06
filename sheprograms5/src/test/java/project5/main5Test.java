149d172a4d2c492c56e78b9a536640a7d7eed2cc
Mohraiel Matta <mohraielmatta@gmail.com>
2022-12-05 22:04:56 -0800

    preorder test

--- sheprograms5/src/test/java/project5/main5Test.java
+++ sheprograms5/src/test/java/project5/main5Test.java
@@ -1,5 +1,5 @@
 package project5;
-
+import java.util.Iterator;
 import org.junit.Test;
 import ADTPackage.*;
 
@@ -232,6 +232,49 @@ public class main5Test
         {
             System.out.print(breadthFirstTraversal.dequeue() + "-> ");
         }
-
     }
+@Test
+
+public void preorderdupe() {
+            // Represent each leaf as a one-node tree
+            BinaryTreeInterface<String> dTree = new BinaryTree<>();
+            dTree.setTree("D", null, null);
+    
+            BinaryTreeInterface<String> dupeTree = new BinaryTree<>();
+            dupeTree.setTree("D", null, null);
+    
+            BinaryTreeInterface<String> gTree = new BinaryTree<>();
+            gTree.setTree("G", null, null);
+    
+            BinaryTreeInterface<String> hTree = new BinaryTree<>();
+            hTree.setTree("H", null, null);
+    
+            BinaryTreeInterface<String> emptyTree = new BinaryTree<>();
+    
+            // Form larger subtrees
+            BinaryTreeInterface<String> eTree = new BinaryTree<>();
+            eTree.setTree("E", dupeTree, gTree); // Subtree rooted at E
+    
+            BinaryTreeInterface<String> bTree = new BinaryTree<>();
+            bTree.setTree("B", dTree, eTree); // Subtree rooted at B
+    
+            BinaryTreeInterface<String> cTree = new BinaryTree<>();
+            cTree.setTree("C", emptyTree, hTree); // Subtree rooted at C
+    
+            BinaryTreeInterface<String> aTree = new BinaryTree<>();
+            aTree.setTree("A", bTree, cTree); // Desired tree rooted at A
+    
+            System.out.println("");
+            // Display root, height, number of nodes
+            System.out.println("\n-------------------------------------------------------");
+            System.out.println("Root of tree contains " + aTree.getRootData());
+            System.out.println("Height of tree is " + aTree.getHeight());
+            System.out.println("Tree has " + aTree.getNumberOfNodes() + " nodes");
+    
+            // Display nodes in preorder
+            System.out.println("A preorder traversal visits nodes in this order:");
+            Iterator<String> preorder =aTree.getPreorderIterator();
+            while (preorder.hasNext())
+            System.out.print(preorder.next() + " ");
+            System.out.println();}
 }
